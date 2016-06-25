4339eb310402b783826313d598ec344ed20fbc1c
hantzo <ghantzaras@gmail.com>
2016-06-25 19:26:02 +0300

    Creating and starting the containers. Printin list of newly created ones

--- lib/containers.py
+++ lib/containers.py
@@ -1,4 +1,27 @@
+import importlib
+try:
+    importlib.import_module('requests')
+except ImportError:
+    import pip
+    pip.main(['install', 'requests'])
+finally:
+    globals()['requests'] = importlib.import_module('requests')
 
+def container_operations(url,action,id):
 
-def container_operations(conf):
-    pass
+    # request endpoint and url
+    endpoint = '/containers/{}/{}'.format(id,action)
+    rest_point = url + endpoint
+
+    r = requests.post(rest_point)
+
+def get_info(url,id):
+
+    endpoint = '/containers/{}/json'.format(id)
+    rest_point = url + endpoint
+
+    r = requests.get(rest_point)
+
+    data = [r.json()['Id'],r.json()['Name'].replace('/',''),r.json()['State']['Status'],list(r.json()['Config']['ExposedPorts'].keys()),r.json()['Config']['Image']]
+
+    return data
--- lib/docker.py
+++ lib/docker.py
@@ -1,15 +1,15 @@
 import subprocess
 import pip
 import importlib
-packages=['requests','termcolor','time']
+import json
+import containers
+packages=['requests','termcolor','time','texttable']
 for package in packages:
     try:
         importlib.import_module(package)
-        print 'Imported {}'.format(package)
     except ImportError:
         import pip
         pip.main(['install', package])
-        print 'Installed {}'.format(package)
     finally:
         globals()[package] = importlib.import_module(package)
 
@@ -21,7 +21,7 @@ def build(conf):
     endpoint = '/build?t=' + conf['image_name']
     rest_point = url + endpoint
 
-    print termcolor.colored('\tBuilding image "{}" from directory "{}"... \n\t\tThis might take a second'.format(conf['image_name'],conf['directory']), 'green')
+    print termcolor.colored('Building image "{}" from directory "{}"... \n\t(This might take a while)'.format(conf['image_name'],conf['directory']), 'blue')
 
     # file to post in request
     tar = 'tar cvf application.tar -C {} .'.format(conf['directory'])
@@ -36,10 +36,46 @@ def build(conf):
 
     #print type(r.status_code())
     if r.status_code == 200:
-        print termcolor.colored('\tImage built successfully!', 'green')
+        print termcolor.colored('Image built successfully!', 'green')
+
+    create_containers(conf)
+
+
 
 def create_containers(conf):
-    pass
+
+    print termcolor.colored('Creating "{}" containers from image "{}"... '.format(conf['num_instances'],conf['image_name']), 'blue')
+
+    # request endpoint and url
+    url = conf['url']
+    endpoint = '/containers/create'
+    rest_point = url + endpoint
+
+    headers = {"Content-Type": "application/json"}
+    with open(conf['container_spec']) as data_file:
+        data = json.load(data_file)
+
+    data['Image'] = conf['image_name']
+
+    print termcolor.colored('Created {} containers!'.format(conf['num_instances']), 'green')
+
+    ids =[]
+    container_data = []
+    container_data.append(['ID','Name','Status','Ports','Image'])
+
+    for i in range(1, int(conf['num_instances'])+1):
+
+        r = requests.post(rest_point, headers=headers, data=json.dumps(data))
+
+        containers.container_operations(url,'start',r.json()['Id'])
+        time.sleep(0.5)
+
+        container_data.append(containers.get_info(url,r.json()['Id']))
+
+    t = texttable.Texttable()
+    t.add_rows(container_data)
+    print t.draw()
+
 
 def list(conf):
     pass
